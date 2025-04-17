c7a579495b24589bde8562f6f00f4b242951cb64
Godawela <godawelaus.21@uom.lk>
2025-04-17 13:43:22 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from "react";
+import LoveReasons from "./components/LoveReasons";
+import SurpriseButton from "./components/SurpriseButton";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-pink-100 to-pink-200">
+      <h1 className="text-4xl font-bold text-rose-700 mt-10">Hi Babe 💌</h1>
+      <LoveReasons />
+      <SurpriseButton />
     </div>
   );
 }
